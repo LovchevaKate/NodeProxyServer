77269bc1560bb7ba9c5eeb520f357498b9462431
LovchevaKate <katelovcheva@yandex.by>
2024-10-14 20:40:37 +0300

    add logic for response

--- index.js
+++ index.js
@@ -20,7 +20,25 @@ app.get("/meteors", async (request, response) => {
   try {
     const result = await axios.get(url);
 
-    response.json(result.data);
+    const data = Object.values(result.data.near_earth_objects).flatMap(
+      (meteors) =>
+        meteors.map((item) => ({
+          id: item.id,
+          name: item.name,
+          diameter:
+            (item.estimated_diameter.meters.estimated_diameter_min +
+              item.estimated_diameter.meters.estimated_diameter_max) /
+            2,
+          is_potentially_hazardous_asteroid:
+            item.is_potentially_hazardous_asteroid,
+          close_approach_date_full:
+            item.close_approach_data[0].close_approach_date_full,
+          relative_velocity:
+            item.close_approach_data[0].relative_velocity.kilometers_per_second,
+        }))
+    );
+
+    response.json(data);
   } catch (error) {
     response.status(500).json({ error: "Faild to fetch data." });
   }
