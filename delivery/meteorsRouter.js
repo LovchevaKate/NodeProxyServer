dfe54dd50f74555c45d000c4c15f7ce3e5c6201b
LovchevaKate <katelovcheva@yandex.by>
2024-10-15 15:01:09 +0300

    add nunjucks

--- delivery/meteorsRouter.js
+++ delivery/meteorsRouter.js
@@ -11,7 +11,7 @@ meteorRouter.get("/meteors", async (request, response) => {
     }
 
     const meteors = await fetchMeteors(date, count, wereDangerousMeteors);
-    return response.json(meteors);
+    response.render("home.html", { meteors });
   } catch (error) {
     return response.status(500).json({ error: "Faild to fetch data." });
   }
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
 import meteorRouter from "./delivery/meteorsRouter.js";
+import nunjucks from "nunjucks";
 
 dotenv.config();
 const { PORT } = process.env;
 
 const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
 
 app.use(meteorRouter);
 
