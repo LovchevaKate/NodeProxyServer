1431d534941628fc619fb7f057002aec5dd60245
LovchevaKate <katelovcheva@yandex.by>
2024-10-22 22:26:51 +0300

    small refactor

--- helper/errorHandler.js
+++ helper/errorHandler.js
@@ -1,17 +1,17 @@
-import HttpError from "./httpError.js";
+import HttpError from './httpError.js'
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     return res.status(err.statusCode).json({
-      status: "error",
-      message: err.message || "An error occurred",
-    });
+      status: 'error',
+      message: err.message || 'An error occurred'
+    })
   }
 
   return res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-};
+    status: 'error',
+    message: 'Internal server error'
+  })
+}
 
-export default errorHandler;
+export default errorHandler
