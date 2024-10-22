67a44eb4bb01cfb62cfed34b8dd31953ddc60cea
Kate Lovcheva <31647082+LovchevaKate@users.noreply.github.com>
2024-10-22 12:46:35 +0300

    Task-11: Implement an endpoint of type POST (#8)
    * create post endpoint
    
    * add errorHandler
    
    * fix comments
    
    * small fixes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
 import meteorRouter from "./delivery/meteorsRouter.js";
+import bodyParser from "body-parser";
 import nunjucks from "nunjucks";
+import errorHandler from "./helper/errorHandler.js";
 
 dotenv.config();
 const { PORT } = process.env;
 
 const app = express();
+app.use(bodyParser.json());
 
 nunjucks.configure("views", {
   autoescape: true,
@@ -14,6 +17,7 @@ nunjucks.configure("views", {
 });
 
 app.use(meteorRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
