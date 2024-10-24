f73d36741179416594bb869742110241397462f6
Kate Lovcheva <31647082+LovchevaKate@users.noreply.github.com>
2024-10-24 10:18:46 +0300

    Task-12 - Add validation (#9)
    * create post endpoint
    
    * create validator
    
    * add validation to the routers
    
    * add errorHandler
    
    * fix comment

--- delivery/meteorsRouter.js
+++ delivery/meteorsRouter.js
@@ -1,27 +1,38 @@
 import express from "express";
 import fetchMeteors from "../useCases/fetchMeteors.js";
 import getRoverImage from "../useCases/getRoverImage.js";
+import getMeteorSchema from "../validation/schemas/getMeteorSchema.js";
+import validateRequest from "../validation/validator.js";
+import postRoverImageSchema from "../validation/schemas/postRoverImageSchema.js";
 
 const meteorRouter = express.Router();
 
-meteorRouter.get("/meteors", async (request, response, next) => {
-  try {
-    const { date, count, wereDangerousMeteors } = request.query;
-    const meteors = await fetchMeteors(date, count, wereDangerousMeteors);
-    response.render("meteor.njk", { meteors });
-  } catch (error) {
-    next(error);
+meteorRouter.get(
+  "/meteors",
+  validateRequest(getMeteorSchema, "query"),
+  async (request, response, next) => {
+    try {
+      const { date, count, wereDangerousMeteors } = request.query;
+      const meteors = await fetchMeteors(date, count, wereDangerousMeteors);
+      response.render("meteor.njk", { meteors });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-meteorRouter.post("/roverImage", async (request, response, next) => {
-  try {
-    const { userId, userName, userApiKey } = request.body;
-    const image = await getRoverImage(userApiKey);
-    return response.json({ photo: image });
-  } catch (error) {
-    next(error);
+meteorRouter.post(
+  "/roverImage",
+  validateRequest(postRoverImageSchema, "body"),
+  async (request, response, next) => {
+    try {
+      const { userId, userName, userApiKey } = request.body;
+      const image = await getRoverImage(userApiKey);
+      return response.json({ photo: image });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 export default meteorRouter;
