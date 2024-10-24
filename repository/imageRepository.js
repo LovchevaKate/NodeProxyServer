30f9ae4fbe35ed841d6d1dd27a005794a79b5815
LovchevaKate <katelovcheva@yandex.by>
2024-10-24 10:17:39 +0300

    fix comment

--- repository/imageRepository.js
+++ repository/imageRepository.js
@@ -3,7 +3,6 @@ import { config } from "../config.js";
 
 const getRoverImages = async (userApiKey) => {
   const url = `${config.nasaApiPhotoUrl}?sol=${config.sol}&camera=${config.camera}&api_key=${userApiKey}`;
-  console.log(url);
   const result = await axios.get(url);
   return result.data;
 };
